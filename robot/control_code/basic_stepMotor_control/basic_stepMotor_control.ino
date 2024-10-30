7682e6f309fcfc1f803d8c16e10ba162f3a87951
Jimin <tonnossi@gmail.com>
2024-10-30 15:50:35 +0900

    스텝 모터 기본 제어 코드

---
+++ robot/control_code/basic_stepMotor_control/basic_stepMotor_control.ino
@@ -0,0 +1,28 @@
+#include <Stepper.h>
+#define STEPS 100
+Stepper stepper_1(STEPS, 4,3,5,6);
+Stepper stepper_2(STEPS, 8,9,10,11);
+Stepper stepper_3(STEPS, A5,A3,A4,A2);
+
+int previous = 0;
+
+void setup() {
+  pinMode(A3, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+
+  stepper_1.setSpeed(200);
+  stepper_2.setSpeed(200);
+  stepper_3.setSpeed(200);
+}
+
+void loop() {
+  int val = analogRead(0);
+
+  stepper_1.step(val - previous);
+  stepper_2.step(val - previous);
+  stepper_3.step(val - previous);
+
+  previous = val;
+}
